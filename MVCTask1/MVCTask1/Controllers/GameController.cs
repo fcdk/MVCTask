fb6c7a0256b49b4b1240a9f01052977884995f71
Maksym Hrytsaiuk <maksym_hrytsaiuk@epam.com>
2016-03-18 23:54:33 +0200

    GameController is implementing, user can get all games

---
+++ MVCTask1/MVCTask1/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MVCTask1Model;
+
+namespace MVCTask1.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public GameController()
+        {
+            _unitOfWork = new UnitOfWork();
+        }
+
+        public JsonResult Games()
+        {
+            return Json(_unitOfWork.Games.GetAllGames().Select(game => new{ game.Name, game.Description }), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
