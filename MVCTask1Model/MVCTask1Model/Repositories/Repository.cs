c36ed0f8509b28a16bcc30dc20d8c8b9c9bed614
Maksym Hrytsaiuk <maksym_hrytsaiuk@epam.com>
2016-03-25 22:15:22 +0200

    generic Repository is implemented

---
+++ MVCTask1Model/MVCTask1Model/Repositories/Repository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data.Entity;
+using MVCTask1EF;
+using MVCTask1Model.RepositoryInterfaces;
+
+namespace MVCTask1Model.Repositories
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly MVCTask1Entities _dbEntities;
+
+        protected Repository(MVCTask1Entities context)
+        {
+            _dbEntities = context;
+        }
+
+        protected MVCTask1Entities DbEntities
+        {
+            get { return _dbEntities; }
+        }
+
+        public TEntity GetByKey(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentException("key argument must not be null and empty");
+
+            TEntity entity = null;
+
+            entity = _dbEntities.Set<TEntity>().Find(key);
+
+            if(entity == null)
+                throw new InvalidOperationException(string.Format("{0} with ID={1} was not found in the DB", typeof(TEntity).Name, key));
+
+            return entity;
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _dbEntities.Set<TEntity>().Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbEntities.Set<TEntity>().Attach(entity);
+            _dbEntities.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (_dbEntities.Entry(entity).State == EntityState.Detached)
+            {
+                _dbEntities.Set<TEntity>().Attach(entity);
+            }
+            _dbEntities.Set<TEntity>().Remove(entity);
+        }
+
+        public void Delete(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentException("key argument must not be null and empty");
+
+            var entityToDelete = _dbEntities.Set<TEntity>().Find(key);
+            if (entityToDelete != null)
+            {
+                Delete(entityToDelete);
+            }
+            else throw new InvalidOperationException(string.Format("{0} with ID={1} was not found in the DB", typeof(TEntity).Name, key));
+        }
+    }
+}
--- MVCTask1Model/MVCTask1Model/RepositoryInterfaces/IGameRepository.cs
+++ MVCTask1Model/MVCTask1Model/RepositoryInterfaces/IGameRepository.cs
@@ -5,11 +5,7 @@ namespace MVCTask1Model.RepositoryInterfaces
 {
     public interface IGameRepository
     {
-        void Create(string name, string description);
-        void Update(string key, string name, string description);
-        void Delete(string key);
         IEnumerable<Game> GetAllGames();
-        Game GetGameByKey(string key);
         IEnumerable<Game> GetGamesByGenre(string genreName);
         IEnumerable<Game> GetGamesByPlatformType(string platformType);
     }
